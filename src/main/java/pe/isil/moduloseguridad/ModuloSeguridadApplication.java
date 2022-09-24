7e333a09539ed2b6e006c791ddd8fd932a410624
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2022-09-23 21:10:28 -0500

    primer commit

---
+++ src/main/java/pe/isil/moduloseguridad/ModuloSeguridadApplication.java
@@ -0,0 +1,16 @@
+package pe.isil.moduloseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloSeguridadApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ModuloSeguridadApplication.class, args);
+
+		System.out.println("Hola mundo");
+	}
+
+}
